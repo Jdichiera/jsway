ced40b5b6ed85e2a090f5249c6d2c1f3210828e2
Jeramy Dichiera <jeramy.dichiera@gmail.com>
2021-10-08 06:33:54 -0400

    Uncomment code

--- ch04/main.js
+++ ch04/main.js
@@ -1,18 +1,18 @@
 // ex 1
-// const rotations = prompt('How many times should the carousel turn?');
-// console.log(`Launching carousel. It will turn ${rotations} times.`);
+const rotations = prompt('How many times should the carousel turn?');
+console.log(`Launching carousel. It will turn ${rotations} times.`);
 
-// for (i = 1; i <= rotations; i++) {
-// 	console.log(` --== rotation ${i} ==-- `)
-// }
+for (i = 1; i <= rotations; i++) {
+	console.log(` --== rotation ${i} ==-- `)
+}
 
-// ex 4
-// const input = prompt('Enter a number between 2 and 9 and I will display the associated multiplication table.');
-// if (input > 1 && input < 10) {
-// 	for(i = 0; i <= 10; i++) {
-// 		console.log(`${input} * ${i} = ${i * input}`);
-// 	}
-// }
+ex 4
+const input = prompt('Enter a number between 2 and 9 and I will display the associated multiplication table.');
+if (input > 1 && input < 10) {
+	for(i = 0; i <= 10; i++) {
+		console.log(`${input} * ${i} = ${i * input}`);
+	}
+}
 
 // ex 6
 for (i = 1; i <= 100; i++ ) {
